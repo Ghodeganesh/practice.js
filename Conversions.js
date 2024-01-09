091cd1c0e41b86f3b89c384211acbb3fcf2b3e77
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-09 18:50:04 +0530

    ConversionOfDataType

---
+++ Conversions.js
@@ -0,0 +1,16 @@
+// CONVERSIONS IN JAVASCRIPT
+
+//1)CONVERSION FROM NUMBER TO STRING
+let age=18;
+console.log(typeof age)
+
+let ageInstr=String(age)
+console.log(typeof ageInstr)
+
+//2)CONVERSION FROM STRING TO NUMBER
+
+let name="19";
+console.log(typeof name)
+
+let num=Number(name)
+console.log(typeof num)
