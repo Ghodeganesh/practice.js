4d92e65d7853794b4452f3ad061ac170415193d9
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-09 18:21:35 +0530

    datatypes in Javascript

---
+++ second.js
@@ -0,0 +1,45 @@
+// DataTypes IN Javascript
+
+//1)Number
+let age=15;
+console.log("The value of age is : ",age);
+console.log(typeof age)
+
+
+//2)String
+let name="Ganesh ghode"
+console.log("The name is : ",name)
+console.log(typeof name)
+
+//3)Symbol
+let my=Symbol("hello")
+console.log(my)
+
+var id = Symbol ( "id" ) ; 
+console.log(id)
+let ss=[id]
+console.log("the symbol is: ", ss)
+// let user = { name : "John" , [ id ] : 123 // not "id": 123 } ;
+
+
+//4)Undefined
+let state1;
+console.log(state1)
+
+
+
+//5)Null
+let state=null;
+console.log(state)
+console.log(typeof state)
+
+//6)BigInt
+
+/*not used for many times */
+
+//7)Boolean
+
+let GameOn=true;
+let Gameof=false;
+console.log(GameOn)
+console.log(Gameof)
