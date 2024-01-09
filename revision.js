87721012cf49272ce69b31057bccd005d0a4499d
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-09 23:52:25 +0530

    revision of Concepts

---
+++ revision.js
@@ -0,0 +1,52 @@
+// console.log("Hello world ");
+
+//DataTypes 
+/*
+              ********String********
+let name="ganesh"
+console.log(name)
+console.log(typeof name)
+console.log(name[2])
+let namToNumber=Number(name)
+console.log(namToNumber);
+let name="ganesh"
+let Surname=" Ghode"
+console.log(name+Surname);
+*/
+
+/*
+          *********Number********
+          
+          let age=19
+          console.log(age)
+          let age1=34
+          if(age>age1){
+              console.log("the age is greater than age2")
+            }
+            else{
+                console.log("the age is lesser than age2")
+            }
+            
+            let numToString=String(age)
+            console.log(typeof numToString)
+
+
+ */
+/*
+
+ ******* Let Var Const ******
+ 
+ 
+ let name="ganesh"
+ var name2="mihir"
+ const name3="tejas"
+ console.log(name)
+ console.log(name2)
+ console.log(name3)
+ 
+ name="amol"
+ console.log(name)
+ name2="adi"
+ console.log(name2)
+ 
+ */
