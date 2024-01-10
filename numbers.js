17142057d2644a913fcb804db639cefd4296ef2c
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-10 12:37:33 +0530

    Math in Js

--- numbers.js
+++ numbers.js
@@ -4,3 +4,12 @@ console.log(balance.toLocaleString('en-IN'))
 console.log(typeof balance.toString())
 console.log(balance.toFixed(3))
 console.log(balance.toPrecision(3))
+
+// ****************Math in javascript******************
+console.log("Math in javscript ")
+console.log(Math)
+// console.log(Math.abs(-88))
+// console.log(Math.round(3.6))
+// console.log(Math.ceil(4.7))
+// console.log(Math.floor(5.7))
+console.log(Math.ceil((Math.random()*100)+1))
