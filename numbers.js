9456903b3ae386909f43beb892aab84ba1c9cc08
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-10 12:06:56 +0530

    Numbers in javascript

---
+++ numbers.js
@@ -0,0 +1,6 @@
+let balance=1234;
+console.log(balance)
+console.log(balance.toLocaleString('en-IN'))
+console.log(typeof balance.toString())
+console.log(balance.toFixed(3))
+console.log(balance.toPrecision(3))
---
+++ strings .js
@@ -0,0 +1,21 @@
+let name="ganesh"
+let  surnaame="ghode"
+console.log(name.toUpperCase())
+console.log(surnaame.toUpperCase())
+console.log(name.trim())
+console.log(surnaame.trim())
+console.log(`My name is: ${name.toUpperCase()} and surname is: ${surnaame.toUpperCase()}`)
+
+let name1=new String("hello")
+console.log(name1.toUpperCase())
+console.log(name.indexOf('g'))
+console.log(name.charAt(3))
+console.log(name.length)
+console.log(surnaame.length)
+
+let name="ganesh"
+let  surnaame="ghode"
+console.log(name.replaceAll("ganesh","Honey"))
+let add="ulhasnagar azad-nagar santoshi mata mandir"
+console.log(add.includes("azad"))
+console.log(add.includes("wadar"))
