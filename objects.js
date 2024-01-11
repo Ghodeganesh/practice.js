78aa164d55a08026e7752b5e21c5062fc37b1219
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-11 23:53:29 +0530

    object.js

--- objects.js
+++ objects.js
@@ -1,19 +1,49 @@
-let obj={
-    name:"ganesh",
-    add:"ulhasnagar",
-    DOB:"20-09-2002",
-    age:21,
-    Degree:"BE ComputerScience"
-}
-let obj2={
-    name:"ganesh",
-    add:"ulhasnagar",
-    DOB:"20-09-2002",
-    age:21,
+// let mySymbol=Symbol("Ganesh")
+// let obj={
+//     name:"ganesh",
+//     add:"ulhasnagar",
+//     DOB:"20-09-2002",
+//     age:21,
+//     [mySymbol]:"hitesh",
+//     Degree:"BE ComputerScience"
+// }
+// console.log(typeof obj.mySymbol)
 
-    Degree:"BE ComputerScience",
-}
-console.log(obj)
-console.log(obj["name"].toUpperCase())
-obj.add="ulhasnagar-2"
-console.log(obj)
+// console.log(obj)
+// console.log(obj["name"].toUpperCase())
+// obj.add="ulhasnagar-2"
+// console.log(obj)
+// obj.greeting =function(){
+//    console.log("hello world greeting")
+// }
+// console.log(obj.greeting())
+// obj.newAdd="Kalyan"
+// console.log(obj)
+
+// let obj2 = {
+//     name: "ganesh ",
+//     surnme: "Ghode",
+//     Degree: {
+//         degree_name: "Computer_Science",
+//         Degree_Duration: "4 Year",
+//         degree_Subject: {
+//             First_year: "6",
+//             second_year: "6",
+//             Third_year: "6",
+//             Fourth_year: "6",
+
+//         }
+
+//     }
+// }
+// console.log(obj2)
+// console.log(obj2.Degree)
+// console.log(obj2.Degree.degree_Subject.First_year)
+// const obj3={...obj, ...obj2}
+// console.log(obj3)
+
+// console.log(Object.keys(obj))
+// console.log(Object.keys(obj2))
+// console.log(Object.values(obj))
+// console.log(Object.values(obj2))
+
