dbb5051314054b77b75465691ace43f034087e15
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-11 22:49:36 +0530

    objects in javascript

---
+++ objects.js
@@ -0,0 +1,19 @@
+let obj={
+    name:"ganesh",
+    add:"ulhasnagar",
+    DOB:"20-09-2002",
+    age:21,
+    Degree:"BE ComputerScience"
+}
+let obj2={
+    name:"ganesh",
+    add:"ulhasnagar",
+    DOB:"20-09-2002",
+    age:21,
+
+    Degree:"BE ComputerScience",
+}
+console.log(obj)
+console.log(obj["name"].toUpperCase())
+obj.add="ulhasnagar-2"
+console.log(obj)
