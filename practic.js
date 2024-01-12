66b33f9783085a15f3951be4981332b88f1bcf61
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-12 20:19:29 +0530

    files

---
+++ practic.js
@@ -0,0 +1,46 @@
+var prompt = require('prompt-sync')();
+// let age=prompt("Enter the age : ")
+// if(age<18){
+//     console.log("you get 20% Discount on Movie Ticket")
+// }
+// else if(age>=18 && age<=65){
+//     console.log("You get Ticket on Normal Price")
+// }
+// if(age>65){
+//     console.log("you get 30% Discount on Movie Ticket")
+// }
+
+
+// console.log("+++++++++  Program For Calculating Area Of Rectangle ++++++++")
+// let lenght=prompt("Enter Lenght of Rectangle : ")
+// let width=prompt("Enter The Width Of Rectangle : ")
+// let Area=lenght*width;
+// console.log("The Area Of Rectangle is : ",Area)
+
+
+
+// let product1={
+//     name:"Burger",
+//     price:399,
+//     size:["small","medium","large"],
+//     discount:"43% on Each Size"
+// }
+// console.log(product1)
+// console.log(product1.size[1])
+
+let guessList = ["Ganesh", "Mihir", "Tejas", "Gaurav", "Swayam"]
+console.log(guessList)
+
+let check = prompt("Checking Name of Person : ")
+
+// for (var i = 0; i <= guessList.length; i++) {
+//     if (check == guessList[i]) {
+
+
+//         console.log(`Welcome To Party ${check}`)
+//         break
+
+
+//     }
+
+// }
