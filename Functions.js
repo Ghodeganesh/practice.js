e3b75066329b280c0ee5b642e5de115259fc932f
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-12 17:53:04 +0530

    function in js

--- Functions.js
+++ Functions.js
@@ -22,14 +22,21 @@
 //     console.log(`HEllo ${anyUser.username} login on ${anyUser.date} for our wbesite`)
 // }
 // helloUser(user)
-function handleObject(anyUser) {
-    console.log( `Hello ${anyUser.username} for loggin Your DOB is ${anyUser.dob} and your email is ${anyUser.email}`)
-}
-handleObject({
-    username: "ganesh",
-    dob: "20-09-2002",
-    email:"ganeshghode2002@gmail.com"
+// function handleObject(anyUser) {
+//     console.log( `Hello ${anyUser.username} for loggin Your DOB is ${anyUser.dob} and your email is ${anyUser.email}`)
+// }
+// handleObject({
+//     username: "ganesh",
+//     dob: "20-09-2002",
+//     email:"ganeshghode2002@gmail.com"
 
-}
+// }
 
-)
+// )
+function handleArray(anyArray){
+    return anyArray
+}
+let myArray=[1,2,3,4,5,6,67,7,8,9]
+// console.log(handleArray(myArray))
+let result=handleArray(myArray)
+console.log(myArray)
