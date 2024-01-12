c19134dce93585819cde83428754fa6ba1f366b1
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-12 16:59:06 +0530

    functions in javascript

---
+++ Functions.js
@@ -0,0 +1,13 @@
+console.log("hello world We Are Lerning Functions iN Depth")
+
+function myName(name,surname){
+    console.log(`My Nmae is ${name} and Surname is ${surname
+    }`)
+}
+myName("Ganesh","Ghode")
+
+myName({
+    name:"ganesh",
+    surname:"ghode"
+
+})
