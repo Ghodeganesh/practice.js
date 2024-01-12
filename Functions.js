3d5c74c15ad8a8ccf8273d88bfca6a251b7eea4f
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-12 17:43:55 +0530

    function with methods

--- Functions.js
+++ Functions.js
@@ -1,13 +1,35 @@
-console.log("hello world We Are Lerning Functions iN Depth")
+// function helloPrint(){
+//     console.log("HEllo Guys Welcome To Our WebSite")
+// }
+// helloPrint()
+// function addTwoNumbers(num1,num2){
+//     return num1+num2
+// }
 
-function myName(name,surname){
-    console.log(`My Nmae is ${name} and Surname is ${surname
-    }`)
+// console.log("The Value of Two Numbers Is : ",addTwoNumbers(3,4))
+
+// function addMultiNumbers(...num){
+//     return num;
+// }
+
+// console.log("The Value of Two Numbers Is : ",addMultiNumbers(3,4,5,6))
+
+// let user={
+//     username:"ganesh",
+//     date:"12-01-2024"
+// }
+// function helloUser(anyUser){
+//     console.log(`HEllo ${anyUser.username} login on ${anyUser.date} for our wbesite`)
+// }
+// helloUser(user)
+function handleObject(anyUser) {
+    console.log( `Hello ${anyUser.username} for loggin Your DOB is ${anyUser.dob} and your email is ${anyUser.email}`)
 }
-myName("Ganesh","Ghode")
+handleObject({
+    username: "ganesh",
+    dob: "20-09-2002",
+    email:"ganeshghode2002@gmail.com"
 
-myName({
-    name:"ganesh",
-    surname:"ghode"
+}
 
-})
+)
