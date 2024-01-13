9acb693484e3380bebfd4cf65cfa25297523d11c
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-13 13:15:14 +0530

    IFFE IN JS

---
+++ IFFE.js
@@ -0,0 +1,16 @@
+// IMMEDIATLY ENVOKING FUNCTION EXPRESION
+let name="ganesh ghode"
+
+(
+    function name(name){
+        // const name="Ganesh"
+        console.log(`Hello My Name is ${name}`)
+    }
+    )("Ganesh");
+    
+(
+    function surname(surname){
+        // const name="Ganesh"
+        console.log(`Hello My surname is ${surname}`)
+    }
+    )("Ghode")
