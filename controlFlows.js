546f76a240fecb1cc9d016b5b5764f6613b9c694
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-16 10:08:40 +0530

    controlFlows

---
+++ controlFlows.js
@@ -0,0 +1,11 @@
+// let name="ganesh"
+// if (name=="manish"){
+//     console.log(`my name is ${name}`)
+// }else{
+//     console.log(`My name is ${name}`)
+// }
+
+//****** Ternary Operator ***** */
+// let balanced=10000;
+// balanced>=5000?console.log("true"):console.log("false");
+
