c4fcf1fb8bd549243272c85a100214c5b8672cd2
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-16 19:22:38 +0530

    advanced loops in js

--- controlFlows.js
+++ controlFlows.js
@@ -9,3 +9,17 @@
 // let balanced=10000;
 // balanced>=5000?console.log("true"):console.log("false");
 
+// let arr=[" ganesh "," mihir "," tejas "," gaurav "," prathamesh "," pranav "]
+// arr.forEach((items,index,char)=>{
+//     console.log(`name of group members  ${index} is ${items} and ${char} `)
+// })
+
+// let arr=[" ganesh "," mihir "," tejas "," gaurav "," prathamesh "," pranav "]
+// for(let val in arr){
+//     console.log(`arr values are : ${val} and Ans is ${arr[val]}`)
+// }
+
+let arr=[" ganesh "," mihir "," tejas "," gaurav "," prathamesh "," pranav "]
+for(let val of arr){
+    console.log(`values of arr ${val}`)
+}
