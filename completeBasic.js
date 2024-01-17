823b699409cae2fc76553d9078793b01c5cc78be
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-17 23:32:14 +0530

    practice

---
+++ completeBasic.js
@@ -0,0 +1,54 @@
+// practice for Javascript 
+
+//****** Strings ****** */
+// console.log("Hello World")
+// let name="ganesh"
+// console.log(name)
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// let newname="     ganesh ghode    "
+// console.log(newname)
+// console.log(newname.trim())
+// console.log(name.charAt(3))
+// console.log(name.indexOf(g))
+
+//***** Arrays***** 
+// let arr=[1,2,3,4,5,6,7,8]
+// console.log(arr)
+// let arr1=[1,2,3,[4,5,6,7],8]
+// console.log(arr1)
+// // console.log(arr.push(99))
+// // console.log(arr)
+// // let arr2=[...arr,...arr1]
+// // console.log(arr2)
+// // console.log(arr.indexOf(4))
+// // console.log(arr1.includes(9))
+// console.log(arr1.flat(Infinity))
+
+
+//****** Objects *******
+// let details={
+//     age:21,
+//     name:"mihir",
+//     degree:"computer_Science"
+// }
+// // console.log(details)
+// details.city="ulhasnagar-2"
+// // console.log(details)
+// let details2={
+//     age:20,
+//     name:"ganesh",
+//     degree:"computer_Science"
+// }
+// let total={...details, ...details2}
+// console.log(total)
+// let sym=Symbol("Ganesh")
+
+// let details2={
+//      age:20,
+//      [sym]:"Ganesh",
+//      name:"ganesh",
+//      degree:"computer_Science"
+// }
+// console.log( details2)
+// console.log(details2["age"])
