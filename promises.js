3f97eb16bd6e59326367cb18371b67036679ff03
ganesh ghode <ganeshghode2002@gmail.com>
2024-01-24 23:52:50 +0530

    updated

--- promises.js
+++ promises.js
@@ -11,39 +11,45 @@
 // new Promise(function(resolved,reject){
 //     setTimeout(function(){
 //         console.log("Hello World")
-//         resolved()
-//     },1000)
-// }).then(function(){
-//     console.log("promise Two Resolved")
-// })
+// //         resolved()
+// //     },1000)
+// // }).then(function(){
+// //     console.log("promise Two Resolved")
+// // })
 
 
-// let promiseThree=new Promise(function(resolved,reject){
-//    setTimeout(function(){
+// // let promiseThree=new Promise(function(resolved,reject){
+// //    setTimeout(function(){
  
-//     resolved({username:"ganesh",email:"ganeshghode",age:"20"})
-//    },1000)
-// })
-// promiseThree.then(function(user){
-//         console.log(user)
-//         return user.username
-// })
+// //     resolved({username:"ganesh",email:"ganeshghode",age:"20"})
+// //    },1000)
+// // })
+// // promiseThree.then(function(user){
+// //         console.log(user)
+// //         return user.username
+// // })
 
-let promiseFive = new Promise(function(resolved,reject){
-    setTimeout(function(){
-        let error=false;
-        if(!error){
-            resolved({username:"ganesh",city:"ulhasnagar"})
-        }else{
-            reject("Error Occured!")
-        }
-    },2000)
-})
-promiseFive.then(function(user){
-    console.log(user)
-    return user.username
-}).then(function(username){
-    console.log("the username is : ",username)
-}).catch(function(){
-    console.log("erro occured")
-})
+// let promiseFive = new Promise(function(resolved,reject){
+//     setTimeout(function(){
+//         let error=false;
+//         if(!error){
+//             resolved({username:"ganesh",city:"ulhasnagar"})
+//         }else{
+//             reject("Error Occured!")
+//         }
+//     },2000)
+// })
+// promiseFive.then(function(user){
+//     console.log(user)
+//     return user.username
+// }).then(function(username){
+//     console.log("the username is : ",username)
+// }).catch(function(){
+// //     console.log("erro occured")
+// // })
+// async function logMovies() {
+//     const response = await fetch("https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch");
+//     const movies = await response.json();
+//     console.log(movies);
+//   }
+  
